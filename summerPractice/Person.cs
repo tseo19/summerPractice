fa77f9c5ac7eed789316a2bba92fffc9877f1506
EGOR TSAMTSUROV <tseo.19@uni-dubna.ru>
2021-06-30 13:48:25 +0300

    Added class Person

---
+++ summerPractice/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace summerPractice
+{
+    class Person
+    {
+        string firstName;
+        string lastName;
+
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        public string LastName => this.lastName;
+        public string FirstNaame => this.firstName;
+        public override string ToString()
+        {
+            return this.firstName + " " + this.lastName;
+        }
+    }
+}
--- summerPractice/Program.cs
+++ summerPractice/Program.cs
@@ -6,7 +6,8 @@ namespace summerPractice
     {
         static void Main(string[] args)
         {
-            
+            Person person = new Person("Oleg", "Ivanov");
+            Console.WriteLine(person.ToString());
         }
     }
 }
