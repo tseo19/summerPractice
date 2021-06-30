a79bcdad7c45a1aed843c2f55e9a5ef3d38cd337
tseo19 <86715509+tseo19@users.noreply.github.com>
2021-06-30 13:50:17 +0300

    Added Id to class Person

--- summerPractice/Person.cs
+++ summerPractice/Person.cs
@@ -8,17 +8,20 @@ namespace summerPractice
     {
         string firstName;
         string lastName;
+        int id;
 
-        public Person(string firstName, string lastName)
+        public Person(string firstName, string lastName, int id)
         {
             this.firstName = firstName;
             this.lastName = lastName;
+            this.id = id;
         }
         public string LastName => this.lastName;
         public string FirstNaame => this.firstName;
+        public int Id => this.id
         public override string ToString()
         {
-            return this.firstName + " " + this.lastName;
+            return this.id + " " + this.firstName + " " + this.lastName;
         }
     }
 }
